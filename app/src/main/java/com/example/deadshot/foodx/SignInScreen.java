d93cc10d8993490387180dc3b2d9a7ceed2d582c
omgitsdeadshot <41427093+omgitsdeadshot@users.noreply.github.com>
2018-09-26 15:04:57 +0500

    Adding files in project 'FoodX2' to Git...

---
+++ app/src/main/java/com/example/deadshot/foodx/SignInScreen.java
@@ -0,0 +1,13 @@
+package com.example.deadshot.foodx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_screen);
+    }
+}
